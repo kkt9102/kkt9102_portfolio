3f90afa8543fef136ed99fe1287ea73313128a73
kkt9102 <kkt9102@gmail.com>
2022-01-02 22:20:17 +0900

    test

---
+++ src/pages/portfolio/reserve_info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function reserve_info() {
+    return (
+        <>
+            <div className="page_tit">스타벅스 리저브</div>
+        </>
+    )
+}
+
+export default reserve_info;
