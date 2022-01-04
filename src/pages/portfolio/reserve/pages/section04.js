42b11fc8d145de1683671a3de4dab0fd51976346
kkt9102 <kkt9102@gmail.com>
2022-01-05 03:25:17 +0900

    해상도조절 잘못하고 작업했었네.... 큰일이네...

--- src/pages/portfolio/reserve/pages/section04.js
+++ src/pages/portfolio/reserve/pages/section04.js
@@ -1,10 +1,12 @@
 import React from "react";
+import sc_4_tit from '../resource/img/sc_4_tit.png';
 
 function Section04() {
     return (
         <>
             <section id="section_04">
                 <div className="con">
+                    <img src={sc_4_tit} className="sc_tit_img"></img>
                 </div>
             </section>
         </>
