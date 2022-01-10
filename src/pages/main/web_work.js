4007c6b5a95e337d0f083addad51d5485eeaad50
kkt9102 <kkt9102@gmail.com>
2022-01-10 21:50:33 +0900

    포폴 초기화

--- src/pages/main/web_work.js
+++ src/pages/main/web_work.js
@@ -5,7 +5,7 @@ function WebWork(){
 
     return (
         <>
-            <Link to ="/Reserve">리저브</Link>
+            <Link to ="/Reserve">스타벅스 리저브 (Re:Design)</Link>
             <Link to ="/clean">부패방지 3단계</Link>
             <Link to ="/Acrc">국민권익위원회 홈페이지·업무포털 전면개편</Link>
         </>
