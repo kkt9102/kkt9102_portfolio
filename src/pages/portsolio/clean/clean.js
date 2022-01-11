84f27e738e5fe76a683a19277d929f95563af569
kkt9102 <kkt9102@gmail.com>
2022-01-12 04:00:10 +0900

    포폴 사업진행부분 추가

--- src/pages/portsolio/clean/clean.js
+++ src/pages/portsolio/clean/clean.js
@@ -1,21 +1,67 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import style from "../../../resource/css/main_style.module.css";
+
+// img
+import main_img from "../../../resource/img/clean/clean_main.png";
+import main_board from "../../../resource/img/clean/clean_board.png";
+import main_sub from "../../../resource/img/clean/clean_sub_list.png";
+import main_pop from "../../../resource/img/clean/clean_sub_pop.png";
 
 function Clean() {
 
     return(
+        <>
+        <div className={style.go_home}>
+            <Link to="/">홈으로</Link>
+        </div>
         <div className={style.clean}>
-            <h2>부패방지 종합정보시스템 구축3단계</h2>
-            <div className={style.cont_box}>
-                <div className={style.sub_tit}>국민권익위원회 | 부패방지 3단계 고도화 사업</div>
-                <div className={style.sub_txt}>
-                    <ul>
-                        <li>퍼블리셔로 취업 후 처음으로 투입 된 공공기관 프로젝트입니다.</li>
-                        <li>국민권익위원회가 운영하는 부패·공익 신고 홈페이지 입니다. 해당 프로젝트는 고도화 사업이었으며 저는 프로젝트 후반 개발이 끝난 항목에 대한 퍼블리싱 지원으로 투입된 프로젝트 입니다.</li>
-                    </ul>
+            <div className={style.con}>
+                <h2>부패방지 종합정보시스템 구축3단계</h2>
+                <div className={`${style.cont_box} ${style.cont_1}`}>
+                    <div className={style.sub_tit}>국민권익위원회 | 부패방지 3단계 고도화 사업</div>
+
+                    <div className={style.sub_txt}>
+                        <div>
+                            <p>퍼블리셔로 취업 후 처음으로 투입 된 공공기관 프로젝트입니다.</p>
+                            <p>국민권익위원회가 운영하는 부패·공익 신고 홈페이지 입니다.</p>
+                            <p>해당 프로젝트는 고도화 사업이었으며 저는 프로젝트 후반 개발이 끝난 항목에 대한 퍼블리싱 지원으로 투입된 프로젝트 입니다.</p>
+                            <p>홈페이지 구축 사업에서 신고서를 작성하는 페이지들의 서브코딩과 반응형 작업, 청렴포털 내에 있는 청렴마당 홈페이지 탭메뉴 퍼블리싱 등을 진행하였습니다.</p>
+                            <p>비록 구축사업 마지막 단계에서 투입된 프로젝트였지만 처음올 개발자, 디자이너와 협업하여 진행한 사업이었습니다.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${style.cont_box} ${style.cont_2}`}>
+                    <div className={style.txt_box}>
+                        <ul className="flex">
+                            <li className="flex">프로젝트 기여도</li>
+                            <li className={style.progress_bar}></li>
+                        </ul>
+                        <ul>
+                            <li><a href="https://www.clean.go.kr/" target="_blank" title="청렴포털 홈페이지 새창으로 바로가기">청렴포털 홈페이지 바로가기 (https://www.clean.go.kr)</a></li>
+                        </ul>
+                    </div>
+                    <div className={style.img_box}>
+                        <ul className={style.img_list}>
+                            <li>
+                                <img src={main_img}></img>
+                            </li>
+                            <li>
+                                <img src={main_board}></img>
+                            </li>
+                            <li>
+                                <img src={main_sub}></img>
+                            </li>
+                            <li>
+                                <img src={main_pop}></img>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
