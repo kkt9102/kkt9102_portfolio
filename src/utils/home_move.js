fcbd9e9ad786165d6b9ee3b34ef03a4a4ff3a123
kkt9102 <kkt9102@gmail.com>
2022-02-13 04:39:19 +0900

    위치 스크롤 표시

--- src/utils/home_move.js
+++ src/utils/home_move.js
@@ -1,6 +1,8 @@
+import { parseHTML } from "jquery";
 import React from "react";
 import { Link } from "react-router-dom";
 
+import {useScroll} from './scrollMove';
 
 function HomeMV() {
     const btn_style = {  
@@ -19,9 +21,22 @@ function HomeMV() {
         fontWeight: 600,
         color: '#' + 111111
     }
+    const {scrollY} = useScroll();
+    const PageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const progressBar = {
+        position: 'fixed',
+        width: (scrollY/PageHeight) * 100 + '%',
+        height: 2,
+        left: 0,
+        top: 0,
+        background: '#' + 141414
+    }
+        
+    
     return (
         <>
             <div className="go_home" style={btn_style}>
+            <div style={progressBar}></div>
                 <Link to="/" style={btn_link}>
                     <div>홈으로</div>
                 </Link>
