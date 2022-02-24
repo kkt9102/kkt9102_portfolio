d18be0cd19abbf216489ebbcdfbfc6f6e2c4100d
kkt9102 <kkt9102@gmail.com>
2022-02-25 03:32:37 +0900

    이메일 라이브러리 페이지 활용

--- src/pages/main/components/contect.js
+++ src/pages/main/components/contect.js
@@ -1,44 +1,48 @@
-import React from "react";
+import React,{ useCallback } from "react";
+import useInput from "../../../utils/useInput";
 import { Link } from "react-router-dom";
 import { RiKakaoTalkFill } from "react-icons/ri";
+import emailjs from '@emailjs/browser';
 
 const Contect = () => {
 
+    // e-mail
+    const [name, onChangeName] = useInput(''); 
+    const [email, onChangeEmail] = useInput(''); 
+    const [contents, onChangeContents] = useInput(''); 
+    
+    const sendEmail = useCallback((e) => { 
+        e.preventDefault(); const inputNum = e.target.childElementCount - 1;
+        const data = new FormData(e.target); 
+        const entries = data.entries(); let failNum = 0; 
+        for (let i = 0; i < inputNum; i++) { 
+            const next = entries.next(); 
+            const key = next.value[0];
+            const value = next.value[1]; 
+            if (!value) { 
+                failNum++; 
+                alert(`${key}이(가) 비어있습니다.`);
+                break; 
+            } 
+        } if (!failNum) { 
+            emailjs.sendForm( 'service_mg5nlfc', 'template_2xziz8w', e.target, 'user_do6P99tYNFqedeqA4YKqe' )
+            .then(
+                (result) => { 
+                    console.log('result.text', result.text);
+                    alert('메일이 발송되었습니다. 3~4일내로 답변을 드리겠습니다.');
+             }, 
+                (error) => { 
+                    console.log(error.text);
+                    alert('메일 발송이 실패했습니다. 다시 시도하시거나 아래의 g-mail 을 이용해주세요.')
+            }); 
+        } }, 
+        []);
     return(
         <>
             <section id='section_04' className='section main_sc_4'>
                 <div className='con'>
-                    <div className='cont_box'>
+                    <div className='cont_box flex flex_wrap_wrap'>
                         <div className='sc_tit flex flex_jc_c'>Contect Me</div>
-                        <div className="sub_tit">저에대해 조금 더 알고 싶으시다면 메일이나 오픈톡으로 연락주세요!</div>
-                        {/* <div className='contect_me_form'>
-                            <form action="" name="SendEmail">
-                                <input type="hidden" name="MyName" value="김기태"></input>
-                                <input type="hidden" name="MyEmail" value="creater@kakao.com"></input>
-                                <div className='contect_header flex'>
-                                    <div className='from_name'>
-                                        <div>Name</div>
-                                        <input type="text" name="from_name" id="from_name" placeholder="성함 or 회사명" title="보내는 분의 성함이나 회사명을 입력해주세요."></input>
-                                    </div>
-                                    <div className='email_box'>
-                                        <div>E-Mail Address</div>
-                                        <input type="text" name="email" id="email" placeholder="답장을 받으실 이메일을 입력해주세요." title="답장을 받으실 이메일을 입력해주세요."></input>
-                                    </div>
-                                </div>
-                                <div className='email_contents_box'>
-                                    <div>E-Mail contents</div>
-                                    <textarea placeholder="내용을 입력해주세요." title="보내실 메일의 내용을 입력해주세요."></textarea>
-                                </div>
-                                <div className='contect_btn flex flex_jc_c flex_ai_'>
-                                    <button>보내기</button>
-                                </div>
-                            </form>
-                        </div> */} 
-                        <div className="link_box">
-                            <div>
-                                <Link to="/Resume" target="_blank">이력서</Link>
-                            </div>
-                        </div>
                         <div className='link_list'>
                             <ul className='flex flex_jc_c flex_wrap_wrap'>
                                 <li className="send_mail">
@@ -48,6 +52,27 @@ const Contect = () => {
                                     <a href="https://open.kakao.com/o/swDf3n0c" target="_blank" rel="noopener noreferrer" title="카카오톡 오픈체팅"><RiKakaoTalkFill/></a>
                                 </li>
                             </ul>
+                        </div>                  
+                        <div className='contect_me_form'>
+                            <form name="SendEmail" onSubmit={sendEmail}>
+                                <div className='contect_header flex'>
+                                    <div className='from_name'>
+                                        <div>Name</div>
+                                        <input type="text" name="name" id="from_name" value={name} onChange={onChangeName} placeholder="성함 or 회사명" title="보내는 분의 성함이나 회사명을 입력해주세요."></input>
+                                    </div>
+                                    <div className='email_box'>
+                                        <div>E-Mail Address</div>
+                                        <input type="email" name="email" id="email" value={email} onChange={onChangeEmail} placeholder="답장을 받으실 이메일을 입력해주세요." title="답장을 받으실 이메일을 입력해주세요."></input>
+                                    </div>
+                                </div>
+                                <div className='email_contents_box'>
+                                    <div>E-Mail contents</div>
+                                    <textarea placeholder="내용을 입력해주세요." value={contents} onChange={onChangeContents} title="보내실 메일의 내용을 입력해주세요." name="contents"></textarea>
+                                </div>
+                                <div className='contect_btn flex flex_jc_c flex_ai_'>
+                                    <input type="submit" value="보내기" />
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
