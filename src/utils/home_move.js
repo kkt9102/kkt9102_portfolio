da6b0bb5c281657e8a09b4df6ed982ed38d523b8
kkt9102 <kkt9102@gmail.com>
2022-04-01 08:46:57 +0900

    design edit

--- src/utils/home_move.js
+++ src/utils/home_move.js
@@ -1,47 +1,27 @@
-import { parseHTML } from "jquery";
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { AiOutlineHome } from 'react-icons/ai';
 
 import {useScroll} from './scrollMove';
 
 function HomeMV() {
-    const btn_style = {  
-        position: 'fixed',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        width: 100 + '%',
-        background: '#fefefe',
-        zIndex: 2
-    }
-    const btn_link = {
-        padding:1 + 'rem',
-        border: 0.1 + 'rem solid' + '#eeeeee',
-        background: '#fefefe',
-        fontSize: 1.5 + 'rem',
-        fontWeight: 600,
-        color: '#' + 111111
-    }
-
     const {scrollY} = useScroll();
     const PageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPer = (scrollY/PageHeight) * 100;
     const progressBar = {
         position: 'fixed',
         width: (scrollY/PageHeight) * 100 + '%',
-        height: 2,
         left: 0,
-        top: 0,
-        background: '#' + 141414
+        top: 0
     }
     
     const PageWidth = document.documentElement.scrollWidth;
     
     return (
         <>
-            <div className="go_home" style={btn_style}>
-                <div style={progressBar}></div>
-                <Link to="/" style={btn_link}>
+            <div className={`${scrollPer > 30 ? 'y_move' : '' } go_home`}>
+                <div className={`${scrollPer > 30 ? 'y_move' : '' } progress`} style={progressBar}></div>
+                <Link to="/" tabIndex='0'>
                     { PageWidth < 420 ? <AiOutlineHome/> : <div>홈으로</div>}  
                 </Link>
             </div>
